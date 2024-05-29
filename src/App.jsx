7e89367e2ca204846138ee8bc5c52ec72a3c8b7f
MSRossii <marcelosrossi13@gmail.com>
2024-05-29 00:39:43 -0300

    create const showValue

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 
 export default function App() {
-  let variable = 0
-  const [state, setState] = useState(0)
-  const ref = useRef(0)
+  let variable = 0;
+  const [state, setState] = useState(0);
+  const ref = useRef(0);
 
   const showValue = () => {
     alert(`
-    Variável: ${}
-    `)
-  }
+    Variável: ${variable}
+    Ref: ${ref.current}
+    State: ${state}
+    `);
+  };
   return (
     <div>
       <h2>Conhecendo o useRef</h2>
