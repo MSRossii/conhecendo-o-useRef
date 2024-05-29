88eb32ff04a2a65244782c8615ee3b1a50843ff1
MSRossii <marcelosrossi13@gmail.com>
2024-05-29 00:39:02 -0300

    create state and ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+
 export default function App() {
+  let variable = 0
+  const [state, setState] = useState(0)
+  const ref = useRef(0)
+
+  const showValue = () => {
+    alert(`
+    Variável: ${}
+    `)
+  }
   return (
     <div>
       <h2>Conhecendo o useRef</h2>
